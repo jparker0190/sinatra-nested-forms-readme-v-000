820ffdfe6af54b10e2108bbfc0821b20b7b25d7e
Joseph Parker <jparker010190@gmail.com>
2018-05-27 00:35:43 +0000

    automatically backed up by learn

--- models/student.rb
+++ models/student.rb
@@ -1,13 +1,13 @@
 attr_reader :name, :grade
- 
+
  STUDENTS = []
- 
+
   def initialize(params)
     @name = params[:name]
     @grade = params[:grade]
     STUDENTS << self
   end
- 
+
   def self.all
     STUDENTS
   end
